345b6766a45ea941effdaf9533133e5cfb92710d
dfghj <mishenko9n@yandex.ru>
2026-02-03 19:15:17 +0300

    переделал чуть код

--- MAIN.py
+++ MAIN.py
@@ -1,15 +1,21 @@
-def plus(a , d ):
-    return a + d
 
-print(plus(3,4))
+def cfll():
 
-def misnys(a , d ):
-    return a - d
+a = float(input("Введите первое число: "))
+d = float(input("Введите второе число: "))
 
-print(misnys(6,3))
+print("\nВыберите операцию:")
+print("1 - Сложение")
+print("2 - Вычитание")
+print("3 - Деление")
+o = input("Введите номер операции 1-3: ")
 
-
-def delenie(a , d ):
-    return a / d
-
-print(delenie(15,3))
+if o == "1":
+    result = plus(a, d)
+    print(f"\nРезультат сложения: {a} + {d} = {result}")
+elif o == "2":
+    result = misnys(a, d)
+    print(f"\nРезультат вычитания: {a} - {d} = {result}")
+elif o == "3":
+    result = delenie(a, d)
+    print(f"\nРезультат деления: {a} / {d} = {result}")
